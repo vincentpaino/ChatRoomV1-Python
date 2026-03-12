a3784be4be10c3aeb8e597eea866757bcfaf5671
Vincent Paino <vincentpaino11@gmail.com>
2026-03-12 15:07:52 -0500

    added structure to server.py and socket API

--- src/server.py
+++ src/server.py
@@ -1,1 +1,35 @@
 import socket
+
+# 1 + last 4 digits of student ID
+SERVER_PORT = 19489
+
+# Use this when running client/server on the same device
+SERVER_IP = "127.0.0.1" 
+
+# create an INET, STREAMing socket
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# bind the socket to a public host, and a well-known port
+serversocket.bind((socket.gethostname(), SERVER_PORT))
+
+# become a server socket
+serversocket.listen(5)
+
+# When we read the data from users.txt on the server start, we store it in two arrays respectively
+UserID=["","",""]
+Password=["","",""]
+
+if serversocket:
+    print("My chat room server. Version One.")
+    print("\n")
+    
+else:
+    print("Error: Could not create server socket please retry again.")
+
+### FUNCTIONS WRITTEN HERE ###
+
+def read_user_pass(UserID=UserID, Password=Password):
+    
+
+
+
